14bae723a5ae59c25acd6b83b37e2b64e0961315
oriav <sauvanon.guillaume@gmail.com>
2017-03-21 21:14:03 +0100

    update

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -4,13 +4,10 @@ angular.module('app')
         $scope.question = "";
         $scope.nextQuestion = function() {
             QuizzService.get().then(function(res) {
-                console.log(res.data);
                 $scope.question = (res.data.results[0].question);
-                console.log(res.data);
                 $scope.answers = res.data.results[0].incorrect_answers;
                 let index = Math.round(Math.random() * (res.data.results[0].incorrect_answers.length - 1));
                 $scope.answers.splice(index, 0, res.data.results[0].correct_answer);
-                console.log($scope.answers);
 
             });
         };
@@ -18,10 +15,12 @@ angular.module('app')
         $scope.nextQuestion();
     });
 
-var index = []; // declare it outside document.ready
-$(document).ready(function() {
-    $('.sal').click(function(i) {
-        values.push($(this).val()); // add value to array
-        console.log(index)
-    });
-});
+
+
+
+
+//une partie = un nombre de questions = total de points
+//profil score = addition du nombre de points de toutes les parties
+//Bonus :
+//stat : meilleur catégorie, points par catégories/difficultés
+//Leaderboard : classement profil score by order
