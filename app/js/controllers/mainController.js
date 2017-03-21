623bec6af0f7b3640ba80a6f5c65dd9095418450
oriav <sauvanon.guillaume@gmail.com>
2017-03-21 17:28:19 +0100

    random answers

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -6,8 +6,22 @@ angular.module('app')
             QuizzService.get().then(function(res) {
                 console.log(res.data);
                 $scope.question = (res.data.results[0].question);
+                console.log(res.data);
+                $scope.answers = res.data.results[0].incorrect_answers;
+                let index = Math.round(Math.random() * (res.data.results[0].incorrect_answers.length - 1));
+                $scope.answers.splice(index, 0, res.data.results[0].correct_answer);
+                console.log($scope.answers);
+
             });
         };
 
         $scope.nextQuestion();
     });
+
+var index = []; // declare it outside document.ready
+$(document).ready(function() {
+    $('.sal').click(function(i) {
+        values.push($(this).val()); // add value to array
+        console.log(index)
+    });
+});
